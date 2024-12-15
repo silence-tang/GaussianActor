3d60abb1d28391ab51fb7dc9c917be9a44ef6dab
PolarisT <56212320+silence-tang@users.noreply.github.com>
2024-12-15 17:46:07 +0800

    Create ckpts.py

---
+++ ckpt/ckpts.py
@@ -0,0 +1,21 @@
+ZJUMOCAPCkpt = {
+    "CoreView_313": {
+        '3d': '/home/xxx/GaussianActor/ckpt/3d_313.ckpt',
+        '2d': '/home/xxx/GaussianActor/ckpt/2d_313.ckpt',
+    },
+    
+    "CoreView_315": {
+        '3d': '/home/xxx/GaussianActor/ckpt/3d_315.ckpt',
+        '2d': '/home/xxx/GaussianActor/ckpt/2d_315.ckpt',
+    },
+
+    "CoreView_377": {
+        '3d': '/home/xxx/GaussianActor/ckpt/3d_377.ckpt',
+        '2d': '/home/xxx/GaussianActor/ckpt/2d_377.ckpt',
+    },
+
+    "CoreView_386": {
+        '3d': '/home/xxx/GaussianActor/ckpt/3d_386.ckpt',
+        '2d': '/home/xxx/GaussianActor/ckpt/2d_386.ckpt',
+    },
+}
